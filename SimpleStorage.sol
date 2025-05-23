afbe9e772cfbc7c092909b3bef953179be3c4eeb
votrongthuc <nam55645@st.vimaru.edu.vn>
2025-05-23 14:24:31 +0700

    section 1 https://vbi.openedu.net/en/courses/saga-bootcamp-10231/E5mnylMXFf3HdKQw/LO8PvkXao5nBVJkj

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,17 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
+event NumberAdded(uint256);
+
 // https://github.com/vbi-academy/solidity-basics?tab=readme-ov-file
 contract SimpleStorage{
     // uint, int, bool, string, bytes, address
 
-    uint256 number; //8
+    uint256 public _number; //8
+
+    struct Person{
+        string name;
+        uint256 number;
+    }
+
+    uint256[] public arrayOfNumber; //[22,33,44]
+
+    uint256[5] public fixedArrayNumber;
+
+    Person[] public dynamicArrayOfPerson;
+
+    mapping(string name => uint256 number) public nameToNumber;
 
     function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        _number = newNumber;
+        emit NumberAdded(_number); // khi hàm chạy hoàn tất sẽ sẽ xuất ra event để backend có thể xử lý tiếp tục như gửi mail...
     }
 
     function getNumber() view public returns(uint256) {
-        return number;
+        return _number;
     }
+
+    function pushToArrayOfNumber(uint256 newNumber) public {
+        arrayOfNumber.push(newNumber);
+    }
+
+    function pushToFixedArrayOfNumber(uint256 index,uint256 newNumber) public {
+        fixedArrayNumber[index] = newNumber;
+    }
+
+    function pushToDynamicArrayOfPerson(string memory name,uint256 number) public {
+        dynamicArrayOfPerson.push(Person({name:name,number: number}));
+    }
+
+    function addNumberForName(string memory name,uint256 number) public{
+        nameToNumber[name]=number;
+    }
+
 }
